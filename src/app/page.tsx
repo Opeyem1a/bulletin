fb74a9b198aa0fbadc2fb305ecc4b5072a1639f4
opeyem1a <opeyadeyemi@gmail.com>
2025-08-04 02:47:15 -0600

    edition: 006

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -2,7 +2,7 @@ import { Spacer } from '@/app/(components)/spacer';
 import { Header } from '@/app/(components)/header';
 import { Footer } from '@/app/(components)/footer';
 import { getDocumentTitle } from '@/app/(config)/helpers';
-import { EmailContent005 } from '@/app/(email-content)/005';
+import { EmailContent006 } from '@/app/(email-content)/006';
 
 interface EmailComponentProps {
     editionNumber: number;
@@ -43,7 +43,7 @@ const Email = ({ editionNumber }: EmailComponentProps) => {
                                     <tbody>
                                         <Header editionNumber={editionNumber} />
                                         <Spacer height="28px" />
-                                        <EmailContent005 />
+                                        <EmailContent006 />
                                         <Spacer height="24px" />
                                         <Footer />
                                     </tbody>
@@ -58,7 +58,7 @@ const Email = ({ editionNumber }: EmailComponentProps) => {
 };
 
 const PreviewEmailPage = () => {
-    return <Email editionNumber={5} />;
+    return <Email editionNumber={6} />;
 };
 
 export default PreviewEmailPage;
